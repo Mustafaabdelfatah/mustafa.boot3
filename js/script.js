e86cc567c1eaa3b1e8d5a454f2d4efe394d1ce78
Mustafaabdelfatah <38335068+Mustafaabdelfatah@users.noreply.github.com>
2018-04-13 15:56:58 +0200

    bootstrab3

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*global $ , document*/
+
+$(document).ready(function () {
+    
+    "use strict";
+    
+     // nicescroll
+    
+    $('html').niceScroll();
+     
+    $(".gear").click(function () {
+         
+        $(".color-option").fadeToggle();
+         
+    });
+    
+    
+     
+});
